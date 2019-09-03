a73d80f8ff1ea177c665789f98def3c666154848
Kathrean911 <53656011+Kathrean911@users.noreply.github.com>
2019-09-03 20:32:59 +0200

    A demo chart in place with unrelated data

--- PneumoProject/PneumoAcc/views.py
+++ PneumoProject/PneumoAcc/views.py
@@ -10,6 +10,11 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.views import PasswordChangeView
 from .models import database
+from django.http import HttpResponse
+from .fusioncharts import FusionCharts
+from collections import OrderedDict
+
+
 
 class Post(models.Model):
     pic = models.ImageField(default = "logo.png")
@@ -44,32 +49,103 @@ from .fusioncharts import FusionCharts
 #from models import *
 
 # The `chart` function is defined to generate Column 2D chart from database.
+
 def chart(request):
-    # Create an object for the column2d chart using the FusionCharts class constructor
-    column2d = FusionCharts("column2d", "ex1" , "600", "400", "chart-1", "json",
-         # The data is passed as a string in the `dataSource` as parameter.
-        """{
-               "chart": {
-                  "caption":"Harry\'s SuperMart",
-                  "subCaption":"Top 5 stores in last month by revenue",
-                  "numberPrefix":"$",
-                  "theme":"ocean"
-               },
-               "data": [
-                    {"label":"Bakersfield Central", "value":"880000"},
-                    {"label":"Garden Groove harbour", "value":"730000"},
-                    {"label":"Los Angeles Topanga", "value":"590000"},
-                    {"label":"Compton-Rancho Dom", "value":"520000"},
-                    {"label":"Daly City Serramonte", "value":"330000"}
-                ]
-            }""")
 
-    # returning complete JavaScript and HTML code,
-    # which is used to generate chart in the browsers.
-    return  render(request, 'query.html', {'output' : column2d.render()})
 
+# returning complete JavaScript and HTML code,
+# which is used to generate chart in the browsers.
+    return  render(request, 'query.html',  {'output' : column2D.render(), 'chartTitle': 'Simple Chart Using Array'})
+    # Create your views here.
 def ToQuery(request):
-    return render(request, 'query.html')
+     # Create an object for the Multiseries column 2D charts using the FusionCharts class constructor
+	mscol2D = FusionCharts("stackedColumn2DLine", "ex1" , "600", "400", "chart-1", "json",
+            # The data is passed as a string in the `dataSource` as parameter.
+    """{
+            "chart": {
+            "showvalues": "0",
+            "caption": "Apple's Revenue & Profit",
+            "subCaption": "(2013-2016)",
+            "numberprefix": "$",
+            "numberSuffix" : "B",
+            "plotToolText" : "Sales of $seriesName in $label was <b>$dataValue</b>",
+            "showhovereffect": "1",
+            "yaxisname": "$ (In billions)",
+            "showSum":"1",
+            "theme": "fusion"
+        },
+        "categories": [{
+            "category": [{
+            "label": "2013"
+            }, {
+            "label": "2014"
+            }, {
+            "label": "2015"
+            }, {
+            "label": "2016"
+            }]
+        }],
+        "dataset": [{
+            "seriesname": "iPhone",
+            "data": [{
+            "value": "21"
+            }, {
+            "value": "24"
+            }, {
+            "value": "27"
+            }, {
+            "value": "30"
+            }]
+        }, {
+            "seriesname": "iPad",
+            "data": [{
+            "value": "8"
+            }, {
+            "value": "10"
+            }, {
+            "value": "11"
+            }, {
+            "value": "12"
+            }]
+        }, {
+            "seriesname": "Macbooks",
+            "data": [{
+            "value": "2"
+            }, {
+            "value": "4"
+            }, {
+            "value": "5"
+            }, {
+            "value": "5.5"
+            }]
+        }, {
+            "seriesname": "Others",
+            "data": [{
+            "value": "2"
+            }, {
+            "value": "4"
+            }, {
+            "value": "9"
+            }, {
+            "value": "11"
+            }]
+        }, {
+            "seriesname": "Profit",
+            "plotToolText" : "Total profit in $label was <b>$dataValue</b>",
+            "renderas": "Line",
+            "data": [{
+            "value": "17"
+            }, {
+            "value": "19"
+            }, {
+            "value": "13"
+            }, {
+            "value": "18"
+            }]
+        }]
+    }""")
+
+	return render(request, 'query.html', {'output': mscol2D.render(), 'chartTitle': 'Stacked Column 2D with Line Chart'})
     # Create your views here.
 path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
 path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
